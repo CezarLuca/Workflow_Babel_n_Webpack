5dd7249decbd6dd264d32e9e77c749efdb9aac89
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2023-12-12 13:05:48 +0100

    Modern Workflow with Webpack Tutorial done

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { stat } = require("fs");
 const path = require("path");
 
 module.exports = {
@@ -5,6 +6,27 @@ module.exports = {
   output: {
     path: path.resolve(__dirname) + "/dist/assets/",
     filename: "bundle.js",
-    // publicPath: "/dist/"
+    publicPath: "/assets/",
+  },
+  devServer: {
+    // contentBase: path.resolve(__dirname) + "/dist",
+    // contentBase: path.resulve(__dirname, "dist"),
+    static: path.resolve(__dirname, "dist"),
+    // publicPath: "/assets/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // exclude: /node_modules/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
 };
